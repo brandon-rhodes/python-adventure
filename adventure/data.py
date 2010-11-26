34403439bff88eb7df227080c08859913d9b35d5
Brandon Craig Rhodes <brandon@rhodesmill.org>
2010-11-25 21:13:50 -0500

    Switched to a system of conditions that can be introspected, for debugging.

--- adventure/data.py
+++ adventure/data.py
@@ -1,7 +1,6 @@
 """Parse the original PDP ``advent.dat`` file."""
 
 import os
-import random
 from .model import Message, Move, Object, Room
 
 ADVENT_DAT = os.path.join(os.path.dirname(__file__), 'advent.dat')
@@ -33,27 +32,35 @@ def section1(data, n, line, *etc):
 def section2(data, n, line, *etc):
     make_object(data.rooms, Room, n).short_description += line + '\n'
 
+_last_travel = [0, [0]]  # x and verbs
+
 def section3(data, x, y, *verbs):
+    global _last_travel
+    if _last_travel[0] == x and _last_travel[1][0] == verbs[0]:
+        verbs = _last_travel[1]  # same first verb implies use whole list
+    else:
+        _last_travel = [x, verbs]
+
     m, n = divmod(y, 1000)
     mh, mm = divmod(m, 100)
 
     if m == 0:
-        condition = lambda(state): True
+        condition = None
     elif 0 < m < 100:
-        condition = lambda(state): random.randint(0, 100) < m
+        condition = ('%', m)
     elif m == 100:
-        condition = lambda(state): not state.is_dwarf()
+        condition = ('not_dwarf')
     elif 100 < m <= 200:
-        condition = lambda(state): state.is_carrying(mm)
+        condition = ('carrying', mm)
     elif 200 < m <= 300:
-        condition = lambda(s): (s.is_carrying(mm) or s.in_room_with(mm))
+        condition = ('carrying_or_in_room_with', mm)
     elif 300 < m:
-        condition = lambda(state): state.prop[mm] != mh - 3
+        condition = ('prop!=', mm, mh - 3)
 
     if n <= 300:
         action = make_object(data.rooms, Room, n)
     elif 300 < n <= 500:
-        action = n - 300
+        action = n - 300  # special computed goto?
     else:
         action = make_object(data.messages, Message, n - 500)
 
--- adventure/model.py
+++ adventure/model.py
@@ -9,13 +9,27 @@ class Move(object):
 
     def __repr__(self):
         verblist = [ self.vocabulary[i] for i in self.verbs ]
+        if not self.condition:
+            condition = ''
+        elif self.condition[0] == '%':
+            condition = ' %d%% of the time' % self.condition[1]
+        elif self.condition[0] == 'not_dwarf':
+            condition = ' if not a dwarf'
+        elif self.condition[0] == 'carrying':
+            condition = ' if carrying %s'
+        elif self.condition[0] == 'carrying_or_in_room_with':
+            condition = ' if carrying or in room with %s'
+        elif self.condition[0] == 'prop!=':
+            condition = ' if prop %d != %d' % self.condition[1:]
+        else:
+            condition = ' if X'
         if isinstance(self.action, Room):
             action = 'moves to %r' % self.action.description
         elif isinstance(self.action, Message):
             action = 'prints %r' % self.action.text
         else:
             action = 'special %d' % self.action
-        return '<%s %s>' % ('|'.join(verblist), action)
+        return '<%s%s %s>' % ('|'.join(verblist), condition, action)
 
 class Room(object):
     """A location in the game."""
