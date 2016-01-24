0614754668e6cfd6c48ca9cc55e6624175a4a530
Brandon Rhodes <brandon@rhodesmill.org>
2016-01-24 00:00:22 -0500

    Run README.txt as doctest in a temp directory
    Fixes #6 by not leaving a spare `advent.save` file sitting around in the
    project directly every time the tests are run.  Instead, the file is
    written to a temporary directory that is deleted at the end of the test.

--- adventure/tests/test_walks.py
+++ adventure/tests/test_walks.py
@@ -1,13 +1,25 @@
 import doctest
 import os
+import shutil
+import tempfile
 
 def load_tests(loader, tests, pattern):
-    if os.path.exists('advent.save'):
-        os.unlink('advent.save')  # to avoid an error during README.txt
+    cd = ChdirTemp()
     tests.addTests(doctest.DocFileSuite(
-            '../README.txt', optionflags=doctest.NORMALIZE_WHITESPACE))
+        '../README.txt', optionflags=doctest.NORMALIZE_WHITESPACE,
+        setUp=cd.setup, tearDown=cd.teardown))
     tests.addTests(doctest.DocFileSuite('syntax.txt'))
     tests.addTests(doctest.DocFileSuite('vignettes.txt'))
     tests.addTests(doctest.DocFileSuite('walkthrough1.txt'))
     tests.addTests(doctest.DocFileSuite('walkthrough2.txt'))
     return tests
+
+class ChdirTemp(object):
+    def setup(self, doctest_object):
+        self.old_directory = os.getcwd()
+        self.tmp_directory = tempfile.mkdtemp()
+        os.chdir(self.tmp_directory)
+
+    def teardown(self, doctest_object):
+        os.chdir(self.old_directory)
+        shutil.rmtree(self.tmp_directory)
