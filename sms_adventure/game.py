53bcf2e73e52f73812133d65d78711bc442f1fff
zhammer <zachary_hammer@alumni.brown.edu>
2019-06-29 14:35:48 -0400

    Add GameGateway

---
+++ sms_adventure/game.py
@@ -0,0 +1,53 @@
+import io
+import re
+from typing import Optional
+
+import adventure
+from adventure.game import Game
+
+from sms_adventure.definitions import GameGateway as GameGatewayABC
+
+
+class GameGateway(GameGatewayABC):
+    game: Optional[Game]
+
+    def __init__(self):
+        self.game: None
+
+    def start(self) -> str:
+        if self.game:
+            raise GameAlreadyStartedError
+
+        self.game = Game()
+        adventure.load_advent_dat(self.game)
+        self.game.start()
+        return self.game.output
+
+    def resume(self, save: io.BytesIO) -> None:
+        if self.game:
+            raise GameAlreadyStartedError
+
+        self.game = Game.resume(save)
+
+    def do_command(self, command: str) -> str:
+        if not self.game:
+            raise GameNotStartedError
+
+        words = re.findall(r'\w+', command)
+        return game.do_command(words)
+
+    def save(self) -> io.BytesIO:
+        if not self.game:
+            raise GameNotStartedError
+
+        save_data_stream = io.BytesIO()
+        game.t_suspend(verb=None, obj=save_data_stream)
+        return save_data_stream
+
+
+class GameNotStartedError(Exception):
+    message = 'Game has not been started'
+
+
+class GameAlreadyStartedError(Exception):
+    message = 'Game already started'
