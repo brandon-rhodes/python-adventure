8aa2ccb7991394be35a8097e367aa47c17e16b84
zhammer <zachary_hammer@alumni.brown.edu>
2019-06-29 14:31:52 -0400

    Add _includes_words_policy

--- sms_adventure/policies.py
+++ sms_adventure/policies.py
@@ -1,8 +1,9 @@
+import re
 from typing import Func, Iterable, Optional
 
 
 class command:
-    policies = (self._one_line_policy,)
+    policies = (self._one_line_policy, self._includes_words_policy)
 
     @staticmethod
     def invalid_reason(command_text: str) -> Optional[str]:
@@ -16,4 +17,10 @@ class command:
     def _one_line_policy(command_text: str) -> Optional[str]:
         if '\n' in command_text:
             return 'COMMAND CAN ONLY BE ONE LINE.'
+        return None
+
+    @staticmethod
+    def _includes_words_policy(command_text: str) -> Optional[str]:
+        if not re.findall(r'\w+', command)
+            return 'COMMAND MUST CONTAIN AT LEAST ONE WORD.'
         return None
